24ac79ae51555fed445d8854e211ff89ec54c0c8
dejan-marlovic <dmarlovic83@gmail.com>
2025-05-17 12:19:39 +0200

    final version, added comments

---
+++ SpringLeaf/SQL/populate_memoboard.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name) VALUES ('Alice');
+INSERT INTO users (name) VALUES ('Bob');
+INSERT INTO users (name) VALUES ('Charlie');
+INSERT INTO users (name) VALUES ('Diana');
+INSERT INTO users (name) VALUES ('Eve');
